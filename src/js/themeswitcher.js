8d55bd1a51ba0d0bbe3058f1054db2c7b007f6c9
Nikita Shpanko <shpancore@gmail.com>
2021-07-15 05:41:53 +0300

    refactored to separate js file, but "default theme" is not appropriate

---
+++ src/js/themeswitcher.js
@@ -0,0 +1,83 @@
+export class ThemeSwitcher {
+  static LIGHT = 'light-theme';
+  static DARK = 'dark-theme';
+
+  static Controller = class {
+    #elem;
+    #elemEvent;
+    #elemProperty;
+    #themeProperty;
+
+    constructor(elem, elemEvent, elemProperty, themeProperty) {
+      this.#elem = typeof elem === 'string' ? document.querySelector(elem) : elem;
+      this.#elemEvent = elemEvent;
+      this.#elemProperty = elemProperty;
+      this.#themeProperty = themeProperty;
+    }
+
+    reportTo(themeObj) {
+      this.#elem.addEventListener(this.#elemEvent, () => {
+        themeObj[this.#themeProperty] = this.#elem[this.#elemProperty];
+      });
+    }
+
+    updateFrom(themeObj) {
+      this.#elem[this.#elemProperty] = themeObj[this.#themeProperty];
+    }
+  };
+
+  #elem;
+  #controller;
+  #themes;
+  #localParam;
+  #name;
+  constructor(
+    elem,
+    controller,
+    themes = { light: ThemeSwitcher.LIGHT, dark: ThemeSwitcher.DARK },
+    localParam = 'theme',
+    defaultIndex = 0,
+  ) {
+    this.#elem = typeof elem === 'string' ? document.querySelector(elem) : elem;
+    this.#controller = controller;
+    this.#themes = themes;
+
+    this.#controller.reportTo(this);
+
+    this.#localParam = localParam;
+    const localValue = localStorage.getItem(this.#localParam);
+    if (localValue) this.name = localValue;
+    if (!this.name) this.index = defaultIndex;
+  }
+
+  set name(themeName) {
+    if (!(themeName in this.#themes)) return;
+    this.#name = themeName;
+    this.#elem.className = this.#themes[themeName];
+    localStorage.setItem(this.#localParam, themeName);
+    this.#controller.updateFrom(this);
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set index(themeIndex) {
+    themeIndex += 0;
+    if (themeIndex < 0) return;
+    const nameList = Object.keys(this.#themes);
+    if (themeIndex >= nameList.length) return;
+    this.name = nameList[themeIndex];
+    //console.log(nameList);
+  }
+
+  get index() {
+    return Object.keys(this.#themes).indexOf(this.#name);
+  }
+
+  static Checkbox = class extends ThemeSwitcher.Controller {
+    constructor(elem) {
+      super(elem, 'change', 'checked', 'index');
+    }
+  };
+}
